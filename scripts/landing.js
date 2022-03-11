f1440dd5d746238bb8b088bc3157ef31b9f9de29
Cody Oh <codyoferrall@gmail.com>
2022-03-11 08:57:27 -0500

    Added late appear class to central info section on the landing page. Made the surrounding svg imgs appear first, this helps draw attention to the center of the page.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,17 @@
-const sections = document.querySelectorAll('.pre-appear');
-
+const sectionsToAppear = document.querySelectorAll('.pre-appear');
+const sectionsToAppearLate = document.querySelectorAll('.pre-appear-late');
+const leftImg = document.querySelector('.pre-appear-left');
+const rightImg = document.querySelector('.pre-appear-right');
 
 window.addEventListener('load', (e) => {
-    for(block of sections){
-        block.classList.add('appear');
+    for(section of sectionsToAppear){
+        section.classList.add('appear');
     }
+
+    for(section of sectionsToAppearLate){
+        section.classList.add('appear');
+    }
+
+    leftImg.classList.add('appear');
+    rightImg.classList.add('appear');
 })
