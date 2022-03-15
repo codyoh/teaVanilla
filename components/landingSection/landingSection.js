30cd04cd2bb80113b6cd8ab537bcebe315d7d896
Cody Oh <codyoferrall@gmail.com>
2022-03-15 19:58:42 -0400

    Added appearing functionality when scrolling into view.

--- components/landingSection/landingSection.js
+++ components/landingSection/landingSection.js
@@ -11,9 +11,10 @@ function drawVine(){
     
     let screenHeight = window.innerHeight;
 
-    let percentageUpScreen = (middleOfSection / (topOfSection + (screenHeight / 2))) - 0.5;
-    console.log(percentageUpScreen)
-    let drawLength = pathLength * percentageUpScreen;
+    let percentageUpScreen = (middleOfSection / (topOfSection + (screenHeight / 2)) + 0.5);
+    let drawLength = pathLength * (percentageUpScreen -1);
+
+    console.log('percentUpScreen: ', percentageUpScreen)
 
     path.style.strokeDashoffset = pathLength - drawLength;
     path2.style.strokeDashoffset = pathLength - drawLength;
@@ -27,7 +28,23 @@ path2.style.strokeDasharray = pathLength + ' ' + pathLength;
 path.style.strokeDashoffset = pathLength;
 path2.style.strokeDashoffset = pathLength;
 
-window.addEventListener('scroll', drawVine);
+window.addEventListener('scroll', () => {
+    drawVine();
+    let topOfSection = landingSection.getBoundingClientRect().top;
+    let middleOfSection = landingSection.getBoundingClientRect().top + (landingSection.getBoundingClientRect().height / 2);
+    let middleOfScreen = window.innerHeight / 2;
+    let topOfScreen = window.innerHeight;
+
+    if (topOfSection < middleOfScreen) {
+        landingSection.classList.add('appear')
+    }
+
+    if (middleOfSection < 0) {
+        landingSection.classList.remove('appear')
+    }
+
+    console.log('middle of element: ', middleOfSection)
+});
 
 // window.addEventListener('scroll', () => {
 //     var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
