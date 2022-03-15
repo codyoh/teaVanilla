cde3a8928307c4d4496002631025f498ad2ecadd
Cody Oh <codyoferrall@gmail.com>
2022-03-14 22:41:48 -0400

    Fixed positioning of when vines animate to be fully animated in center of screen. Still need to finish their out animation.

--- components/landingSection/landingSection.js
+++ components/landingSection/landingSection.js
@@ -2,26 +2,30 @@ const landingSection = document.querySelector('.landing-section');
 
 
 let path = document.querySelector('path')
+let path2 = document.querySelector('.vine2')
 let pathLength = path.getTotalLength();
 
 function drawVine(){
     let middleOfSection = landingSection.getBoundingClientRect().top - (landingSection.getBoundingClientRect().height / 2);
-    let bottomOfSection = landingSection.getBoundingClientRect().bottom;
+    let topOfSection = landingSection.getBoundingClientRect().top;
     
     let screenHeight = window.innerHeight;
 
-    let percentageUpScreen = middleOfSection / (screenHeight / 2);
+    let percentageUpScreen = (middleOfSection / (topOfSection + (screenHeight / 2))) - 0.5;
     console.log(percentageUpScreen)
     let drawLength = pathLength * percentageUpScreen;
 
     path.style.strokeDashoffset = pathLength - drawLength;
+    path2.style.strokeDashoffset = pathLength - drawLength;
 }
 
 
 
 path.style.strokeDasharray = pathLength + ' ' + pathLength;
+path2.style.strokeDasharray = pathLength + ' ' + pathLength;
 
 path.style.strokeDashoffset = pathLength;
+path2.style.strokeDashoffset = pathLength;
 
 window.addEventListener('scroll', drawVine);
 
